e3ee79e6f53f510d6568891f06fdc6ecfb686335
Niko Dojan <niko201n@edu.easj.dk>
2021-01-04 12:58:48 +0100

    dgfd

--- Activities/SplashActivity.cs
+++ Activities/SplashActivity.cs
@@ -8,11 +8,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Android.Support.V7.App;
 
 namespace TMHelper.Activities
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/tmTheme.Splash", MainLauncher = true, NoHistory = true)]
-    public class SplashActivity : Activity
+    [Activity(Label = "@string/app_name", Theme = "@style/Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
